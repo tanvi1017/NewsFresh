189ab6031584717072cc390d6564c4713363be52
tanvi1017 <tanavi@hipster-dev.com>
2023-01-23 10:50:38 +0530

    calling Json inside fragment SerializedName

--- app/src/main/java/com/tanvi/newsfresh/Model/Article.kt
+++ app/src/main/java/com/tanvi/newsfresh/Model/Article.kt
@@ -1,12 +1,33 @@
 package com.tanvi.newsfresh.Model
 
-data class Article(
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
-)
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Article{
+@SerializedName("source")
+@Expose
+ var source:Source?=null
+    @SerializedName("author")
+    @Expose
+    var author:String?=null
+    @SerializedName("content")
+    @Expose
+    var content:String?=null
+    @SerializedName("description")
+    @Expose
+    var description:String?=null
+    @SerializedName("publishedAt")
+    @Expose
+    var date:String?=null
+
+  @SerializedName("title")
+  @Expose
+  var title:String?=null
+    @SerializedName("url")
+    @Expose
+    var url:String?=null
+
+    @SerializedName("url To Image")
+    @Expose
+    var urlToImage: String?=null
+ }
